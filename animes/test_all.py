02775a180fd9d0f9d792135abc53cdeba32f0e52
marianogg9 <gonzalez.mariano.gabriel@gmail.com>
2023-06-16 18:27:18 +0200

    updated tests

--- animes/test_all.py
+++ animes/test_all.py
@@ -1,8 +1,6 @@
 import pytest
 from . import create_app
 
-authorization = "some API token"
-
 @pytest.fixture()
 def app():
     app = create_app()
@@ -16,28 +14,65 @@ def app():
 def client(app):
     return app.test_client()
 
+def signup(client):
+    response = client.post("/signup",json={"email":"alguna@sarasa.com","password":"1234"})
+    return response
+
+def login(client):
+    signup(client)
+    response = client.post("/login",json={"email":"alguna@sarasa.com","password":"1234"})
+    return response
+
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
 
 def test_signup(client):
-    response = client.post("/signup",json={"email":"alguna@sarasa.com","password":"1234"})
+    response = signup(client)
     assert response.status_code == 200
 
+def test_login_no_signup(client):
+    # no signup first
+    response = client.post("/login",json={"email":"alguna@sarasa.com"})
+    assert response.status_code == 404
+    assert response.json["message"] == "User does not exist."
+
+def test_login_wrong_body(client):
+    signup(client)
+    response = client.post("/login",json={"email":"alguna@sarasa.com"})
+    assert response.status_code == 201
+
 def test_login(client):
-    response = client.post("/login",json={"email":"alguna@sarasa.com","password":"1234"})
+    # signup first
+    signup(client)
+    response = login(client)
     assert response.status_code == 200
 
 def test_all(client):
-    response = client.get("/all",headers={"Authorization": authorization})
-    assert response.status_code == 401 or response.status_code == 402
-    assert response.json["status"] == "fail"
+    # extract auth_token from login method
+    response = client.get("/all",headers={"Authorization": login(client).json["auth_token"]})
+    assert response.status_code == 200
 
 def test_anime(client):
-    response = client.get("/anime/Death Note Rewrite",headers={"Authorization": authorization})
+    response = client.get("/anime/Death Note Rewrite",headers={"Authorization": login(client).json["auth_token"]})
     assert response.status_code == 200
     assert response.json[0]["Members"] == 88699
 
 def test_profile(client):
-    response = client.get("/profile",headers={"Authorization": authorization})
+    response = client.get("/profile",headers={"Authorization": login(client).json["auth_token"]})
     assert response.json["data"]["email"] == "alguna@sarasa.com"
+
+def test_all_not_authorized(client):
+    # no auth_token
+    response = client.get("/all")
+    assert response.status_code == 401
+
+def test_all_wrong_method(client):
+    response = client.post("/all",headers={"Authorization": login(client).json["auth_token"]})
+    assert response.status_code == 405
+
+def test_logout(client):
+    login(client)
+    response = client.post("/logout",headers={"Authorization": login(client).json["auth_token"]})
+    assert response.status_code == 200
+    assert response.json["message"] == "Successfully logged out."
