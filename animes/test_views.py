4b139062e8b4fbc9df5924e680d8920aceaaf012
marianogg9 <gonzalez.mariano.gabriel@gmail.com>
2023-08-29 10:10:36 +0200

    renamed tests

--- animes/test_views.py
+++ animes/test_views.py
@@ -31,7 +31,7 @@ def test_signup(client):
     response = signup(client)
     assert response.status_code == 200
 
-def test_login_no_signup(client):
+def test_login_before_signup(client):
     # no signup first
     response = client.post("/login",json={"email":"alguna@sarasa.com"})
     assert response.status_code == 404
@@ -48,26 +48,26 @@ def test_login(client):
     response = login(client)
     assert response.status_code == 200
 
-def test_all(client):
+def test_get_all(client):
     # extract auth_token from login method
     response = client.get("/all",headers={"Authorization": login(client).json["auth_token"]})
     assert response.status_code == 200
 
-def test_anime(client):
+def test_get_anime(client):
     response = client.get("/anime/Death Note Rewrite",headers={"Authorization": login(client).json["auth_token"]})
     assert response.status_code == 200
     assert response.json[0]["Members"] == 88699
 
-def test_profile(client):
+def test_get_profile(client):
     response = client.get("/profile",headers={"Authorization": login(client).json["auth_token"]})
     assert response.json["data"]["email"] == "alguna@sarasa.com"
 
-def test_all_not_authorized(client):
+def test_get_all_while_not_authorized(client):
     # no auth_token
     response = client.get("/all")
     assert response.status_code == 401
 
-def test_all_wrong_method(client):
+def test_all_with_wrong_method(client):
     response = client.post("/all",headers={"Authorization": login(client).json["auth_token"]})
     assert response.status_code == 405
 
