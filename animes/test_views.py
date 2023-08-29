58397471568240b17ba445288befa8f9b718fd65
marianogg9 <gonzalez.mariano.gabriel@gmail.com>
2023-08-29 12:07:26 +0200

    added conftest.py to define fixtures

--- animes/test_views.py
+++ animes/test_views.py
@@ -1,31 +1,4 @@
-import pytest
-from . import create_app
-
-@pytest.fixture()
-def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True
-    })
-
-    yield app
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
-def signup(client):
-    response = client.post("/signup",json={"email":"alguna@sarasa.com","password":"1234"})
-    return response
-
-def login(client):
-    signup(client)
-    response = client.post("/login",json={"email":"alguna@sarasa.com","password":"1234"})
-    return response
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
+from .conftest import signup,login
 
 def test_signup(client):
     response = signup(client)
